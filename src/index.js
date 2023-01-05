e3578f88455a5e7bf5517cbd147b4585908aaaaa
Ksyusha <74423375+ksen4ik@users.noreply.github.com>
2023-01-05 13:35:51 +0300

    feat: add index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function toReadable (number) {
-  
+    let singleDigits = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let twiceDigits = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    let tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+    let hundred = "hundred";
+
+    if (number == 0) {
+        return "zero";
+    } else if (number > 0 && number < 10) {
+        return singleDigits[number];
+    } else if (number > 9 && number < 20) {
+        return twiceDigits[number-10];
+    } else if (number > 19 && number < 100) {
+        return tens[Math.floor(number/10)] + (number%10 == 0 ? "" : (" " + singleDigits[number%10]));
+    } else if (number > 99 && number < 1000) {
+        return singleDigits[Math.floor(number/100)] + " " + hundred + ((number%100 == 0) ? "" : (number%100 < 10) ? (" " + singleDigits[number%100]) : (number%100 < 20) ? 
+        (" " + twiceDigits[(number%100)-10]) : (number%100 > 19) ? (" " + tens[Math.floor(number%100/10)] + ((number%100%10 > 0) ? (" " + singleDigits[number%100%10]) : "")) : "");
+    }
 }
